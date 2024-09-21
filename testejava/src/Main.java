e266cd6e002e087e6472d701849e75363705dfd3
Luccas Asaphe <luccasasahe@gmail.com>
2024-09-20 21:29:31 -0300

    Initial commit

---
+++ testejava/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args){
+        int x = 10;
+        double y = 20.3297;
+        System.out.println("x = "+x);
+        System.out.printf("y = %.1f%n",y);
+        boolean a = true;
+        //System.in.read(x);
+        if (!a){
+            System.out.println("A");
+        }else {
+            System.out.println("B");
+        }
+
+    }
+}
